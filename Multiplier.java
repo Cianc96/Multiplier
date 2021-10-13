e07936ddf9168c5c156f4baabce2652a9b2163b9
Cianc96 <86387743+Cianc96@users.noreply.github.com>
2021-10-13 14:10:56 +0100

    Create Multiplier.java

---
+++ Multiplier.java
@@ -0,0 +1,25 @@
+//Simple Multiplier of two numbers with user inputs 
+
+import java.util.Scanner;
+ 
+public class QuickMaths{
+   public static void main(String[] args);{
+
+    double num1;
+    double num2;
+
+    Scanner input;
+    input = new Scanner(System.in);
+
+    System.out.print("enter a number:");
+    num1 = input.nextDouble();
+
+    System.out.print("Another number please");
+    num2 = input.nextDouble();
+
+    double result = num1 * num2;
+
+    System.out.println("The result is" + result);
+    input.close();
+    }
+  }
